d6110cfaae0c0fcf658ae858a845e740a40a1ef2
Maxwell Zimmer <zimmermaxwell@gmail.com>
2025-05-28 11:20:03 -0500

    Cleaned up tracked files and added .gitignore

---
+++ Restaurants.API/Program.cs
@@ -0,0 +1,27 @@
+using Restaurants.Infastructure.Extensions;
+using Restaurants.Infastructure.Seeders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+await seeder.Seed();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Restaurants.Application/Restaurants/RestaurauntsService.cs
@@ -0,0 +1,15 @@
+﻿using Restaurants.Domain.Entities;
+namespace Restaurants.Application.Restaurants;
+
+internal class RestaurauntsService
+{
+
+    public Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    {
+        // This is a placeholder for the actual implementation.
+        // In a real application, you would retrieve the list of restaurants from a database or service.
+        return Task.FromResult<IEnumerable<Restaurant>>(new List<Restaurant>());
+
+    }
+}
+
